cef959175edbb250ed2b2ea948e463e96dded5aa
optimisme <optimisme@gmail.com>
2023-09-10 10:15:49 +0200

    First version

---
+++ Release/run.ps1
@@ -0,0 +1,1 @@
+java  --module-path "./lib/javafx-windows/lib" --add-modules javafx.controls,javafx.fxml -cp "Project.jar;.\lib\slf4j-api-2.0.3.jar;.\lib\gson-2.9.1.jar;.\lib\Java-WebSocket-1.5.3.jar;.\lib\slf4j-simple-2.0.3.jar" Main
